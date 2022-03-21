f40af1412280c5d9cfba0b1a14e06d521d2d7b53
Emir Ali Çeken <96144723+emirckn@users.noreply.github.com>
2022-03-21 19:39:08 +0300

    Java-Sayıları Büyükten Küçüğe Sıralayan Program

---
+++ SayılarıBüyüktenKüçüğeSıralama.java
@@ -0,0 +1,41 @@
+package SayılarıBüyüktenKüçüğeSıralama;
+import java.util.Scanner;
+public class SayılarıBüyüktenKüçüğeSıralama {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int num1,num2,num3;
+		
+		System.out.print("1.Sayıyı giriniz:");
+		num1=sc.nextInt();
+		
+		System.out.print("2.Sayıyı giriniz:");
+		num2=sc.nextInt();
+		
+		System.out.print("3.Sayıyı giriniz:");
+		num3=sc.nextInt();
+		
+		if((num1>num2) && (num1>num3))
+		{
+			if(num2>num3)
+				System.out.println("num1 > num2 > num3");
+			else
+				System.out.println("num1 > num3 > num 2");
+		}
+		else if((num2>num1) && (num2>num3))
+		{
+			if(num1>num3)
+				System.out.println("num2 > num1 > num3");
+			else
+				System.out.println("num2 > num3 > num1");
+		}
+		else
+		{
+			if(num1>num2)
+				System.out.println("num3 > num1 > num2");
+			else
+				System.out.println("num3 > num2 > num1");
+		}
+		
+	}
+}
