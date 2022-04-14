23dae0a7493c181ca2e91662c69cfac03c84b11b
Emir Ali Çeken <96144723+emirckn@users.noreply.github.com>
2022-04-14 23:35:15 +0300

    Java- Palindromik Kelimleri Bulan Program

---
+++ PalindromStrign.java
@@ -0,0 +1,23 @@
+package PatikaProjeleri;
+
+public class PalindromStrign {
+
+	public static boolean palindrom(String kelime) {
+		int i=0;
+		int j=kelime.length()-1;
+		while(i<j) {
+			if(kelime.charAt(i)!=kelime.charAt(j)) {
+				return false;
+			}
+			i++;
+			j--;
+		}
+		return true;	
+	}
+	
+	
+	public static void main(String[] args) {
+		System.out.println(palindrom("baba"));
+		System.out.println(palindrom("aba"));	
+	}
+}
